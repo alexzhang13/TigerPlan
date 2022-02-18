a8780d84ac99f3ead7707a9339262d59872d69a3
alexzhang13 <alex.lx.zhang@gmail.com>
2022-02-18 11:32:16 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// Main script that runs when the website launches
+
+// Loading in dependencies
+var express = require('express');
+
+// Initialize express
+var app = express();
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
