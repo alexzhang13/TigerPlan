a62c983c70ec02aa500d3387ea2af98349478510
Lu Esteban <78449978+esteban-lu@users.noreply.github.com>
2022-03-18 12:20:26 -0400

    init TimeBlock CRUD

--- app/routes/group.py
+++ app/routes/group.py
@@ -3,7 +3,7 @@ from app import db
 from models import Group
 
 def create_group(name: str, owner: User, members: list(User)) -> Group:
-    """Create a group."""
+    """Create a group. Returns created group."""
     new_group = Group(name=name,
                       owner=owner,
                       members=members)
@@ -12,19 +12,20 @@ def create_group(name: str, owner: User, members: list(User)) -> Group:
     return new_group
 
 def get_group(id: int) -> Group:
-    """Get a group."""
+    """Get a group. Returns group."""
     return db.session.query(Group).filter(Group.id == id).one()
 
 def update_group(id: int, name: str, members: list(User)) -> Group:
-    """Update a group."""
-    update_group = db.session.query(Group).filter(Group.id == id).one()
+    """Update a group. Returns updated group."""
+    updated_group = db.session.query(Group).filter(Group.id == id).one()
     if name is not None:
-        update_group.name = name
+        updated_group.name = name
     if members is not None:
-        update_group.members = members
+        updated_group.members = members
+    return updated_group
 
 def delete_group(id: int) -> bool:
-    """Delete a group."""
+    """Delete a group. Returns true if successful."""
     del_group = db.session.query(Group).filter(Group.id == id).one()
     db.session.delete(del_group)
     db.session.commit()
---
+++ app/routes/timeblock.py
@@ -0,0 +1,30 @@
+from app.models.models import TimeBlock, User
+from app import db
+from models import Group
+
+def create_timeblock(name: str, user: User) -> TimeBlock:
+    """Create a time block. Returns created time block."""
+    new_tb = TimeBlock(name=name,
+                      user=user)
+    db.session.add(new_tb)
+    db.session.commit()
+    return new_tb
+
+def get_timeblock(id: int) -> Group:
+    """Get a time block. Returns time block."""
+    return db.session.query(TimeBlock).filter(TimeBlock.id == id).one()
+
+def update_timeblock(id: int, name: str) -> TimeBlock:
+    """Update a time block. Returns updated time block."""
+    updated_tb = db.session.query(TimeBlock).filter(TimeBlock.id == id).one()
+    if name is not None:
+        updated_tb.name = name
+    return updated_tb
+
+def delete_timeblock(id: int) -> bool:
+    """Delete a time block. Returns true if successful."""
+    del_tb = db.session.query(TimeBlock).filter(TimeBlock.id == id).one()
+    db.session.delete(del_tb)
+    db.session.commit()
+    # if successfully deleted, del_tb.id should be None
+    return del_tb.id == None
