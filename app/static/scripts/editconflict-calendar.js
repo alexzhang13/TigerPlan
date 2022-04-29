5be74f61d83061570a195be89df457ac581aebff
Andrew Tao <aytao@princeton.edu>
2022-04-29 02:48:07 -0400

    Fixed edit calendar bug

--- app/static/scripts/editconflict-calendar.js
+++ app/static/scripts/editconflict-calendar.js
@@ -9,210 +9,213 @@ let recurringCal;
 let onetimeCal;
 const recurringCalId = "1";
 const onetimeCalId = "2";
+let recurring;
 function renderEditConflictsCalendars() {
-    recurringCal = new tui.Calendar('#recurCalendar', {
-        id: recurringCalId,
-        defaultView: 'week',
-        taskView: false,
-        scheduleView: ['time'],
-        useCreationPopup: true,
-        useDetailPopup: true,
-    });
-    onetimeCal = new tui.Calendar('#onetimeCalendar', {
-        id: onetimeCalId,
-        defaultView: 'week',
-        taskView: false,
-        scheduleView: ['time'],
-        useCreationPopup: true,
-        useDetailPopup: true,
-    });
+    if (recurring) {
+        recurringCal = new tui.Calendar('#recurCalendar', {
+            id: recurringCalId,
+            defaultView: 'week',
+            taskView: false,
+            scheduleView: ['time'],
+            useCreationPopup: true,
+            useDetailPopup: true,
+        });
+        recurringCal.on({
+            'clickMore': function (e) {
+                console.log('clickMore', e);
+            },
+            'clickSchedule': function (e) {
+            },
+            'clickDayname': function (date) {
+                console.log('clickDayname', date);
+            },
+            'beforeCreateSchedule': function (e) {
 
-    recurringCal.on({
-        'clickMore': function (e) {
-            console.log('clickMore', e);
-        },
-        'clickSchedule': function (e) {
-        },
-        'clickDayname': function (date) {
-            console.log('clickDayname', date);
-        },
-        'beforeCreateSchedule': function (e) {
+                // $("#create").fadeIn();
+                saveNewSchedule(e, true);
+            },
+            'beforeUpdateSchedule': function (e) {
+                var schedule = e.schedule;
+                var changes = e.changes;
 
-            // $("#create").fadeIn();
-            saveNewSchedule(e, true);
-        },
-        'beforeUpdateSchedule': function (e) {
-            var schedule = e.schedule;
-            var changes = e.changes;
+                console.log('beforeUpdateSchedule', e);
 
-            console.log('beforeUpdateSchedule', e);
+                var data = {
+                    id: schedule.id,
+                    title: (e.changes && e.changes.title) ? e.changes.title : null,
+                    start: (e.changes && e.changes.start) ? e.changes.start.toUTCString() : null,
+                    end: (e.changes && e.changes.end) ? e.changes.end.toUTCString() : null,
+                }
 
-            var data = {
-                id: schedule.id,
-                title: e.changes.title ? e.changes.title : schedule.title,
-                start: e.changes.start ? e.changes.start.toUTCString() : null,
-                end: e.changes.end ? e.changes.end.toUTCString() : null,
+                var url = '/update_conflict'
+
+                console.log(data)
+                $.ajax({
+                    type: "POST",
+                    url: url,
+                    data: JSON.stringify(data),
+                    dataType: "json",
+                    success: function (response) {
+                        console.log(response);
+                        recurringCal.updateSchedule(schedule.id, schedule.calendarId, changes);
+                    },
+                    error: function (error) {
+                        console.log(error);
+                    }
+                });
+
+                // UPDATE ON DB ON FLASK
+
+                // refreshScheduleVisibility();
+            },
+            'beforeDeleteSchedule': function (e) {
+                console.log('beforeDeleteSchedule', e);
+
+                if (!e.schedule.id) return;
+
+                var url = '/del_conflict/' + e.schedule.id;
+                $.ajax({
+                    type: "POST",
+                    url: url,
+                    dataType: "json",
+                    success: function (response) {
+                        console.log(response);
+
+                        recurringCal.deleteSchedule(e.schedule.id, e.schedule.calendarId);
+                    },
+                    error: function (error) {
+                        console.log(error);
+                    }
+                });
+
+            },
+            'afterRenderSchedule': function (e) {
+                // var schedule = e.schedule;
+                // var element = cal.getElement(schedule.id, schedule.calendarId);
+                // console.log('afterRenderSchedule', element);
+            },
+            'clickTimezonesCollapseBtn': function (timezonesCollapsed) {
+                console.log('timezonesCollapsed', timezonesCollapsed);
+
+                if (timezonesCollapsed) {
+                    recurringCal.setTheme({
+                        'week.daygridLeft.width': '77px',
+                        'week.timegridLeft.width': '77px'
+                    });
+                } else {
+                    recurringCal.setTheme({
+                        'week.daygridLeft.width': '60px',
+                        'week.timegridLeft.width': '60px'
+                    });
+                }
+
+                return true;
             }
 
-            var url = '/update_conflict'
+        });
+    } else {
+        onetimeCal = new tui.Calendar('#onetimeCalendar', {
+            id: onetimeCalId,
+            defaultView: 'week',
+            taskView: false,
+            scheduleView: ['time'],
+            useCreationPopup: true,
+            useDetailPopup: true,
+        });
+        onetimeCal.on({
+            'clickMore': function (e) {
+                console.log('clickMore', e);
+            },
+            'clickSchedule': function (e) {
+            },
+            'clickDayname': function (date) {
+                console.log('clickDayname', date);
+            },
+            'beforeCreateSchedule': function (e) {
 
-            console.log(data)
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: JSON.stringify(data),
-                dataType: "json",
-                success: function (response) {
-                    console.log(response);
-                    recurringCal.updateSchedule(schedule.id, schedule.calendarId, changes);
-                },
-                error: function (error) {
-                    console.log(error);
+                // $("#create").fadeIn();
+                saveNewSchedule(e, false);
+            },
+            'beforeUpdateSchedule': function (e) {
+                var schedule = e.schedule;
+                var changes = e.changes;
+
+                console.log('beforeUpdateSchedule', e);
+
+                var data = {
+                    id: schedule.id,
+                    title: (e.changes && e.changes.title) ? e.changes.title : null,
+                    start: (e.changes && e.changes.start) ? e.changes.start.toUTCString() : null,
+                    end: (e.changes && e.changes.end) ? e.changes.end.toUTCString() : null,
                 }
-            });
 
-            // UPDATE ON DB ON FLASK
+                var url = '/update_conflict'
 
-            // refreshScheduleVisibility();
-        },
-        'beforeDeleteSchedule': function (e) {
-            console.log('beforeDeleteSchedule', e);
+                console.log(data)
+                $.ajax({
+                    type: "POST",
+                    url: url,
+                    data: JSON.stringify(data),
+                    dataType: "json",
+                    success: function (response) {
+                        console.log(response);
+                        onetimeCal.updateSchedule(schedule.id, schedule.calendarId, changes);
+                    },
+                    error: function (error) {
+                        console.log(error);
+                    }
+                });
 
-            if (!e.schedule.id) return;
+                // UPDATE ON DB ON FLASK
 
-            var url = '/del_conflict/' + e.schedule.id;
-            $.ajax({
-                type: "POST",
-                url: url,
-                dataType: "json",
-                success: function (response) {
-                    console.log(response);
+                // refreshScheduleVisibility();
+            },
+            'beforeDeleteSchedule': function (e) {
+                console.log('beforeDeleteSchedule', e);
 
-                    recurringCal.deleteSchedule(e.schedule.id, e.schedule.calendarId);
-                },
-                error: function (error) {
-                    console.log(error);
+                if (!e.schedule.id) return;
+
+                var url = '/del_conflict/' + e.schedule.id;
+                $.ajax({
+                    type: "POST",
+                    url: url,
+                    dataType: "json",
+                    success: function (response) {
+                        console.log(response);
+
+                        onetimeCal.deleteSchedule(e.schedule.id, e.schedule.calendarId);
+                    },
+                    error: function (error) {
+                        console.log(error);
+                    }
+                });
+
+            },
+            'afterRenderSchedule': function (e) {
+                // var schedule = e.schedule;
+                // var element = cal.getElement(schedule.id, schedule.calendarId);
+                // console.log('afterRenderSchedule', element);
+            },
+            'clickTimezonesCollapseBtn': function (timezonesCollapsed) {
+                console.log('timezonesCollapsed', timezonesCollapsed);
+
+                if (timezonesCollapsed) {
+                    onetimeCal.setTheme({
+                        'week.daygridLeft.width': '77px',
+                        'week.timegridLeft.width': '77px'
+                    });
+                } else {
+                    onetimeCal.setTheme({
+                        'week.daygridLeft.width': '60px',
+                        'week.timegridLeft.width': '60px'
+                    });
                 }
-            });
 
-        },
-        'afterRenderSchedule': function (e) {
-            // var schedule = e.schedule;
-            // var element = cal.getElement(schedule.id, schedule.calendarId);
-            // console.log('afterRenderSchedule', element);
-        },
-        'clickTimezonesCollapseBtn': function (timezonesCollapsed) {
-            console.log('timezonesCollapsed', timezonesCollapsed);
-
-            if (timezonesCollapsed) {
-                recurringCal.setTheme({
-                    'week.daygridLeft.width': '77px',
-                    'week.timegridLeft.width': '77px'
-                });
-            } else {
-                recurringCal.setTheme({
-                    'week.daygridLeft.width': '60px',
-                    'week.timegridLeft.width': '60px'
-                });
+                return true;
             }
 
-            return true;
-        }
-
-    });
-    onetimeCal.on({
-        'clickMore': function (e) {
-            console.log('clickMore', e);
-        },
-        'clickSchedule': function (e) {
-        },
-        'clickDayname': function (date) {
-            console.log('clickDayname', date);
-        },
-        'beforeCreateSchedule': function (e) {
-
-            // $("#create").fadeIn();
-            saveNewSchedule(e, false);
-        },
-        'beforeUpdateSchedule': function (e) {
-            var schedule = e.schedule;
-            var changes = e.changes;
-
-            console.log('beforeUpdateSchedule', e);
-
-            var data = {
-                id: schedule.id,
-                title: e.changes.title ? e.changes.title : schedule.title,
-                start: e.changes.start ? e.changes.start.toUTCString() : null,
-                end: e.changes.end ? e.changes.end.toUTCString() : null,
-            }
-
-            var url = '/update_conflict'
-
-            console.log(data)
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: JSON.stringify(data),
-                dataType: "json",
-                success: function (response) {
-                    console.log(response);
-                    onetimeCal.updateSchedule(schedule.id, schedule.calendarId, changes);
-                },
-                error: function (error) {
-                    console.log(error);
-                }
-            });
-
-            // UPDATE ON DB ON FLASK
-
-            // refreshScheduleVisibility();
-        },
-        'beforeDeleteSchedule': function (e) {
-            console.log('beforeDeleteSchedule', e);
-
-            if (!e.schedule.id) return;
-
-            var url = '/del_conflict/' + e.schedule.id;
-            $.ajax({
-                type: "POST",
-                url: url,
-                dataType: "json",
-                success: function (response) {
-                    console.log(response);
-
-                    onetimeCal.deleteSchedule(e.schedule.id, e.schedule.calendarId);
-                },
-                error: function (error) {
-                    console.log(error);
-                }
-            });
-
-        },
-        'afterRenderSchedule': function (e) {
-            // var schedule = e.schedule;
-            // var element = cal.getElement(schedule.id, schedule.calendarId);
-            // console.log('afterRenderSchedule', element);
-        },
-        'clickTimezonesCollapseBtn': function (timezonesCollapsed) {
-            console.log('timezonesCollapsed', timezonesCollapsed);
-
-            if (timezonesCollapsed) {
-                onetimeCal.setTheme({
-                    'week.daygridLeft.width': '77px',
-                    'week.timegridLeft.width': '77px'
-                });
-            } else {
-                onetimeCal.setTheme({
-                    'week.daygridLeft.width': '60px',
-                    'week.timegridLeft.width': '60px'
-                });
-            }
-
-            return true;
-        }
-
-    });
+        });
+    }
 }
 
 function saveNewSchedule(scheduleData, recurring) {
@@ -255,24 +258,8 @@ function saveNewSchedule(scheduleData, recurring) {
             console.log(error);
         }
     });
-
-    // refreshScheduleVisibility();
 }
 
-function refreshScheduleVisibility(cal) {
-    var calendarElements = Array.prototype.slice.call(document.querySelectorAll('#calendarList input'));
-
-    calendarList.forEach(function (calendar) {
-        cal.toggleSchedules(calendar.id, !calendar.checked, false);
-    });
-
-    cal.render(true);
-
-    calendarElements.forEach(function (input) {
-        var span = input.nextElementSibling;
-        span.style.backgroundColor = input.checked ? span.style.borderColor : 'transparent';
-    });
-}
 
 const recurringColor = "#9BD912";
 function renderUserRecurringConflicts(conflicts) {
@@ -310,12 +297,12 @@ function renderUserRecurringConflicts(conflicts) {
 
 const onetimeColor = "#12D3D9";
 function renderUserOnetimeConflicts(conflicts) {
-    if (!recurringCal) {
+    if (!onetimeCal) {
         return;
     }
     let allRenderedUserConflicts = [];
-    let calStart = recurringCal.getDateRangeStart().toDate();
-    let calEnd = recurringCal.getDateRangeEnd().toDate();
+    let calStart = onetimeCal.getDateRangeStart().toDate();
+    let calEnd = onetimeCal.getDateRangeEnd().toDate();
     calEnd.setDate(calEnd.getDate() + 1);
     console.log("Cal start", calStart);
     console.log("Cal end", calEnd);
@@ -351,8 +338,13 @@ function loadUserConflicts() {
                 console.log("An error occured");
             }
             console.log(response);
-            renderUserRecurringConflicts(response.recurringConflicts);
-            renderUserOnetimeConflicts(response.onetimeConflicts);
+            if (recurring) {
+                console.log("Recurring");
+                renderUserRecurringConflicts(response.recurringConflicts);
+            } else {
+                console.log("One time");
+                renderUserOnetimeConflicts(response.onetimeConflicts);
+            }
         },
         error: function (error) {
             console.log(error);
@@ -372,38 +364,42 @@ function setRenderRangeText() {
 }
 
 $(document).ready(function () {
-    // call ajax to load calendar
-    let url = '/load_conflicts'
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+    });
+    // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
+    if (params.type === "onetime") {
+        recurring = false;
+    } else {
+        recurring = true;
+    }
     renderEditConflictsCalendars();
     loadUserConflicts();
-    addCalendar(recurringCal);
-    addCalendar(onetimeCal);
-    $("#onetimeCalendarMenuPrev").on('click', function() {
+    $("#onetimeCalendarMenuPrev").on('click', function () {
         if (!onetimeCal) return;
         onetimeCal.prev();
         setRenderRangeText();
     });
-    $("#onetimeCalendarMenuNext").on('click', function() {
+    $("#onetimeCalendarMenuNext").on('click', function () {
         if (!onetimeCal) return;
         onetimeCal.next();
         setRenderRangeText();
     });
     setRenderRangeText();
-    
-    $("#recurButton").on('click', function() {
-        $("#recurButton").prop('disabled', true);
-        $("#onetimeButton").prop('disabled', false);
-        $("#onetimeSpan").hide();
+
+    if (recurring) {
         $("#recurSpan").show();
+        $("#onetimeButton").show();
+        $("#onetimeButton").attr('href', '/editconflicts?type=onetime');
+        $("#calendarType").html("Recurring");
         recurringCal.render();
-    })
-    $("#onetimeButton").on('click', function() {
-        $("#recurButton").prop('disabled', false);
-        $("#onetimeButton").prop('disabled', true);
-        $("#recurSpan").hide();
-        $("#onetimeSpan").show();
+    } else {
+        $("#onetimeSpan").show();        
+        $("#recurButton").show();
+        $("#recurButton").attr('href', '/editconflicts?type=recur');
+        $("#calendarType").html("One Time");
         onetimeCal.render();
-    })
+    }
 });
 
 /********** HELPER FUNCTIONS: SHOULD BE MOVED TO UTILITIES **********/
